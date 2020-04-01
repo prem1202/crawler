31ea0c0021ed5847cc462d0aeb20e5f83ae4aa2f
prem1202 <61898473+prem1202@users.noreply.github.com>
2020-04-01 18:22:25 +0530

    Update books.py
    error fixing

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -27,9 +27,9 @@ class BooksSpider(scrapy.Spider):
             "//div[@id='product_description']/following-sibling::p/text()"
         ).extract_first()
 	item['image'] = response.xpath(
-            "//div[@class='thumbnail']/following-sibling::img/@href()"
+            "//div[@class='thumbnail']/following-sibling::img/href()"
         ).extract_first()
         item['rate'] = response.css('p.price_color ::text').extract_first()
 	item['availability'] = response.css('p.price_color ::text').extract_first()
-	item['instock'] = response.css('.instock ::html').extract()
+	item['instock'] = response.css('.instock ::text').extract()
         yield item
