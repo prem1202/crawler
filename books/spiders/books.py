9cf2a09844547a172f10720b8f4dcaacbdd0151a
prem1202 <61898473+prem1202@users.noreply.github.com>
2020-04-02 10:36:58 +0530

    Update books.py
    adding jumia data crawler

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -3,20 +3,51 @@ import scrapy
 
 
 class BooksSpider(scrapy.Spider):
-    name = "products"
-    allowed_domains = ["products.jumia.com.ng"]
-    start_urls = [
-        'https://www.jumia.com.ng/health-beauty/',
-    ]
+    name = 'jumia_bots'
+    allowed_domains = ['jumia.com.ng']
+    start_urls = ['https://www.jumia.com.ng/laptops/']
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+        laptops = response.xpath('//a[@class="link"]/@href').extract()
+        for laptop in laptops:
+            yield Request(laptop, callback=self.parse_page)
 
-    def parse_book_page(self, response):
-        item = {}
-        item['name'] = response.css('span.name ::text').extract_first()
-        yield item
+    def parse_page(self, response):
+        title = response.xpath('//h1[@class="title"]/text()').extract_first()
+        product_url = response.url
+        brand = response.xpath(
+            '//div[@class="sub-title"]/a/text()').extract_first()
+        price = '#' + response.xpath(
+            '//span[contains(@class, "price")]/span[@dir="ltr"]/@data-price'
+        ).extract_first()
+        rating1 = response.xpath(
+            '//div[@class="container"]/i/following-sibling::span/text()'
+        ).extract_first()
+        rating2 = response.xpath(
+            '//div[@class="container"]/following-sibling::footer/text()'
+        ).extract_first()
+        rating = rating1 + ': ' + rating2
+        rating = rating.replace(',', '.')
+        image_urls = response.xpath(
+            '//div[@id="thumbs-slide"]/a/@href').extract()
+        description = response.xpath(
+            '//div[@class="product-description"]/text()').extract_first()
+
+        # Validate fields
+        title = field_validator(title)
+        product_url = field_validator(product_url)
+        brand = field_validator(brand)
+        price = field_validator(price)
+        rating = field_validator(rating)
+        image_urls = field_validator(image_urls)
+        description = field_validator(description)
+
+        yield {
+            'title': title,
+            'product_url': product_url,
+            'brand': brand,
+            'price': price,
+            'rating': rating,
+            'image_urls': image_urls,
+            'description': description
+        }
