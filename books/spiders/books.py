4c0b3ace211254a1801ef2977ae7d908b356bd97
prem1202 <61898473+prem1202@users.noreply.github.com>
2020-04-05 00:22:00 +0530

    Update books.py
    error fixing

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -12,7 +12,7 @@ class BooksSpider(scrapy.Spider):
     def parse(self, response):
         for product_url in response.css(".-gallery > a ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(product_url), callback=self.products)
-        next_page = response.css("li.item > a[title='next'] ::attr(href)").extract_first()
+        next_page = response.css("li.item > a[title='Next'] ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
