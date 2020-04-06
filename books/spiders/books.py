c7b973286802f7947bb889cd108b970ad6e76366
prem1202 <61898473+prem1202@users.noreply.github.com>
2020-04-06 10:53:57 +0530

    Update books.py
    getting category

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -12,9 +12,9 @@ class BooksSpider(scrapy.Spider):
     def parse(self, response):
         for product_url in response.css(".-gallery > a ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(product_url), callback=self.products)
-        next_page = response.css("li.item > a[title='Next'] ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+        #next_page = response.css("li.item > a[title='Next'] ::attr(href)").extract_first()
+        #if next_page:
+            #yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
     def products(self, response):
         item = {}
@@ -22,9 +22,11 @@ class BooksSpider(scrapy.Spider):
         item["product_name"] = product.css("h1 ::text").extract_first()
         item['price'] = product.css("div.-mtxs > span ::text").extract_first()
         item['brand'] = response.css(".-fs14.-pvxs > a ::text").extract_first()
+        item['brand2'] = product.css("#add-to-cart ::attr(data-brand)").extract_first()
         #item['link'] = response.css(".link::attr(href)").extract_first()
         item['images'] = response.css("img::attr(data-src)").extract()
         item['sku'] = product.css("#add-to-cart ::attr(data-id)").extract_first()
+        item['categories'] = product.css("#add-to-cart ::attr(data-category)").extract_first()
         item['product_details'] = response.css(".markup.-mhm.-pvl.-oxa::text").extract_first()
         item['key_features'] = response.css(".markup.-pam::text").extract()
         item['specification'] = response.css(".-pvs.-mvxs.-phm.-lsn").extract()
