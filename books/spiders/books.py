7d094972e2630e7557796b92e0acba605fa7073c
prem1202 <61898473+prem1202@users.noreply.github.com>
2020-04-07 10:32:56 +0530

    Update books.py
    error fixing

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -3,7 +3,7 @@ import scrapy
 
 
 class BooksSpider(scrapy.Spider):
-   name = 'konga_bots'
+    name = 'konga_bots'
     allowed_domains = ['konga.com']
     start_urls = [
         'https://www.konga.com/category/laptops-5230'
