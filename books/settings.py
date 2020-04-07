bc3ff315f3a590a06743d31865b754c20b79f3d0
prem1202 <61898473+prem1202@users.noreply.github.com>
2020-04-08 02:11:56 +0530

    Update settings.py
    error fixing

--- books/settings.py
+++ books/settings.py
@@ -10,3 +10,4 @@ HTTPCACHE_ENABLED = True
 DOWNLOAD_HANDLERS = {
     'ftp': None,
 }
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
