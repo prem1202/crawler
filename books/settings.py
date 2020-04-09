1dcf819106be32ef17a3cc0b8dc63fc3f83b876d
prem1202 <61898473+prem1202@users.noreply.github.com>
2020-04-09 11:20:31 +0530

    Update settings.py
    error fixing

--- books/settings.py
+++ books/settings.py
@@ -5,10 +5,10 @@ BOT_NAME = 'books'
 SPIDER_MODULES = ['books.spiders']
 NEWSPIDER_MODULE = 'books.spiders'
 
-#ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
-ROBOTSTXT_OBEY = False
-DOWNLOAD_HANDLERS = {
-    'ftp': None,
-}
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+#ROBOTSTXT_OBEY = False
+#DOWNLOAD_HANDLERS = {
+    #'ftp': None,
+#}
+#USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
